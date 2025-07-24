53d1a982f6f850101bf82caf510d01da06c8106e
Samm Zuest Cooper <koopero@gmail.com>
2025-07-24 16:17:23 -0700

    Cut down readme

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'Blaketronics',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/koopero/Blaketronics' }],
 			sidebar: [
 				{
 					label: 'Lessons',
